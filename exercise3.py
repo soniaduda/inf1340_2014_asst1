c623e0d33b8d0124ac53dc13d36e7c58cfa7a9fe
laurenol <lauren.olar@gmail.com>
2014-09-30 11:40:06 -0400

    Added errors

--- exercise3.py
+++ exercise3.py
@@ -12,6 +12,12 @@ def decide_rps(player1, player2):
                     ("Scissors", "Paper"): 1,
                     ("Scissors", "Scissors"): 0}
 
+    if player1 != "Rock" or player1!= "Paper" or player1 != "Scissors":
+        raise TypeError("Not a valid input")
+
+    if player2 != "Rock" or player1!= "Paper" or player1 != "Scissors":
+        raise TypeError("Not a valid input")
+
     return rps_outcomes.get((player1, player2))
 
 
