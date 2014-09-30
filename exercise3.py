6eecccc0166130ef2d0c8d44609a5df50f527ff2
laurenol <lauren.olar@gmail.com>
2014-09-30 11:27:50 -0400

    Wrote in exercise 3

--- exercise3.py
+++ exercise3.py
@@ -2,5 +2,17 @@
 
 
 def decide_rps(player1, player2):
-    return 1
+    rps_outcomes = {("Rock","Rock"): 0,
+                    ("Rock", "Paper"): 2,
+                    ("Rock", "Scissors"): 1,
+                    ("Paper", "Rock"): 1,
+                    ("Paper", "Paper"): 0,
+                    ("Paper", "Scissors"): 2,
+                    ("Scissors", "Rock"): 2,
+                    ("Scissors", "Paper"): 1,
+                    ("Scissors", "Scissors"): 0}
+
+    return rps_outcomes.get((player1, player2))
+
+
 
